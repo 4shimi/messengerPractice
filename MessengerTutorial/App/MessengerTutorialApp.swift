c932e6969e8139b478b45090ba9b8460a8dae8a9
4shimi <king81597@snu.ac.kr>
2023-08-12 08:57:04 +0900

    until profile

---
+++ MessengerTutorial/App/MessengerTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  MessengerTutorialApp.swift
+//  MessengerTutorial
+//
+//  Created by 정회승 on 2023/08/09.
+//
+
+import SwiftUI
+
+@main
+struct MessengerTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
